9d593c9c894d0c46f1af8f8a2ef48a293b844314
Indra Mahkota <indramahkota1@gmail.com>
2020-02-15 09:50:18 +0700

    Add project files.

---
+++ Controllers/ReviewsController.cs
@@ -0,0 +1,116 @@
+﻿using BooksCatalogueAPI.Data;
+using BooksCatalogueAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksCatalogueAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly MyDatabaseContext _context;
+
+        public ReviewsController(MyDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reviews
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Review>>> GetReview()
+        {
+            return await _context.Review.ToListAsync();
+        }
+
+        // GET: api/Reviews/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Review>> GetReview(int id)
+        {
+            var review = await _context.Review.FindAsync(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return review;
+        }
+
+        // PUT: api/Reviews/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReview(int id, Review review)
+        {
+            if (id != review.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(review).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReviewExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reviews
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Review>> PostReview(Review review)
+        {
+            var newReview = new Review
+            {
+                BookId = review.BookId,
+                ReviewerName = review.ReviewerName,
+                Rating = review.Rating,
+                Comment = review.Comment
+            };
+
+            _context.Review.Add(newReview);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReview", new { id = review.Id }, review);
+        }
+
+        // DELETE: api/Reviews/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Review>> DeleteReview(int id)
+        {
+            var review = await _context.Review.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            _context.Review.Remove(review);
+            await _context.SaveChangesAsync();
+
+            return review;
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Review.Any(e => e.Id == id);
+        }
+    }
+}
